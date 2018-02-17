e30deab837974b285be9cdf9cc8c9799bdc7fbc0
wbigelow <wbigelow@uw.edu>
2018-02-17 15:58:17 -0800

    Commented

--- common.php
+++ common.php
@@ -1,20 +1,37 @@
 <?php
+	# Common functions for the lyric API.
+
+	# returns: a new database connection
 	function get_db() {
 		$db = new PDO("mysql:host=127.0.0.1;dbname=hw7;charset=utf8", "root", "root");
 		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		return $db;
 	}
 
+	# Inserts a lyric into the given database.
+	# params:
+	#		$db: a database connection
+	#		$lyric: a lyric to be inserted
 	function insert($db, $lyric) {
 		$insert = "INSERT INTO Lyrics (lyirc) ";
 		$values = "VALUES ('$lyric');";
 		$db->exec($insert . $values);
 	}
 
+	# Removes a lyric from the given database.
+	# params:
+	#		$db: a database connection
+	#		$lyric: a lyric to be removed
 	function delete($db, $lyric) {
 		$db->exec("DELETE FROM Lyrics WHERE lyric = '$lyric';");
 	}
 
+	# Creates an error response for when a request is made with invalid parameters
+	# params:
+	#		$params: an array of length 1 or 2 of the missing parameters
+	#		$conjoiner: "and" or "or" for when there are two parameters
+	# returns:
+	#		An error respsonse JSON object
 	function missing_param($params, $conjoiner = null) {
 		header("HTTP/1.1 400 Invalid Request");
 		$paramOne = $params[0];
