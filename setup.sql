1f420bbfd9974da88d454e986a04025923033077
wbigelow <wbigelow@uw.edu>
2018-02-17 16:27:50 -0800

    Added setup file for sql

---
+++ setup.sql
@@ -0,0 +1,11 @@
+-- Creates a database, codeday.
+-- Drops any existing database named codeday.
+-- Creates a table, Phrases which stores phrases.
+-- Drops any existing table named Phrases.
+DROP DATABASE IF EXISTS codeday;
+CREATE DATABASE codeday;
+USE codeday;
+DROP TABLE IF EXISTS Phrases;
+CREATE TABLE Phrases(
+	phrase VARCHAR(255) NOT NULL PRIMARY KEY
+);
