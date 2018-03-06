b207654f834c810061c48ce656e5ba7f48e58413
Pradyoth <pvvemul@uw.edu>
2018-03-06 12:16:45 -0800

    Commented

--- GetTracks.py
+++ GetTracks.py
@@ -1,7 +1,10 @@
+# Parses a spreadsheet of song titles and pulls the lyrics from the genius api.
+
 import csv
 import requests
 from bs4 import BeautifulSoup
 
+# Parse csv file and create a dictionary of song lyrics to artist names
 with open('regional-us-daily-2017-07-02.csv', newline='') as csvfile:
     tracksDict = dict()
     for row in csvfile.readlines():
@@ -11,12 +14,14 @@ with open('regional-us-daily-2017-07-02.csv', newline='') as csvfile:
         tracksDict[songName] = artist
     del tracksDict['Track Name']
 
-_URL_API = "https://api.genius.com"
-_URL_SEARCH = "/search"
+# Api information and authorization token
+URL_API = "https://api.genius.com/search"
 headers = {'Authorization': 'Bearer JDndxEZOpwM-t1usfurLT3MnTXhyypjV9445CmtYrVc72yBbMQupyEJB_4BasnvG'}
 
+# Does a genius search of the song path from the api results.
+# Grabs and returns html text on lyrics page.
 def lyrics_from_song_api(song_api_path):
-    song_url = _URL_API + song_api_path
+    song_url = URL_API + song_api_path
     response = requests.get(song_url, headers=headers)
     json = response.json()
     path = json["response"]["song"]["path"]
@@ -27,10 +32,12 @@ def lyrics_from_song_api(song_api_path):
     lyrics = html.find("div", class_="lyrics").get_text()
     return lyrics
 
+# Searches api for song title, checks if artist matches results.
+# Gets lyrics and writes to output file.
 if __name__ == "__main__":
     outputFile = open("Mid2017Lyrics.txt", "w")
     for key in tracksDict:
-        query = _URL_API + _URL_SEARCH
+        query = URL_API
         data = {'q': key}
         response = requests.get(query, data=data, headers=headers)
         json = response.json()
